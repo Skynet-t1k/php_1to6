bd78e56a756904de7c9e8eb437616d75f2926295
Giuseppe Cattano <giuseppe.cattano@gmail.com>
2024-05-19 17:35:20 +0200

    completato primo eserzizio

---
+++ php_01_giuseppe_cattano.php
@@ -0,0 +1,15 @@
+<?php
+$integer = 1;
+$float = 1.11;
+$string = "ciao";
+$boolean = true;
+
+const INTEGER = 1;
+const FLOAT = 1.11;
+const STRING = "ciao";
+const BOOLEAN = true;
+
+var_dump($integer);
+var_dump($float);
+var_dump($string);
+var_dump($booleanger);
